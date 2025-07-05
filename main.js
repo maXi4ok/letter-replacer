506ee1a674dea9008b56bf86028f5885a3e943b3
maXi4ok <mftank22@gmail.com>
2025-07-05 16:55:07 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,61 @@
+
+const keyboardMap = {
+    f: 'а', F: 'А',
+    j: 'й', J: 'Й',
+    k: 'л', K: 'Л',
+    v: 'м', V: 'М',
+    e: 'у', E: 'У',
+    r: 'к', R: 'К',
+    t: 'е', T: 'Е',
+    y: 'н', Y: 'Н',
+    g: 'п', G: 'П',
+    h: 'р', H: 'Р',
+    n: 'т', N: 'Т',
+    u: 'г', U: 'Г',
+    i: 'ш', I: 'Ш',
+    o: 'щ', O: 'Щ',
+    p: 'з', P: 'З',
+    '[': 'х', '{': 'Х',
+    ']': 'ї', '}': 'Ї',
+    a: 'ф', A: 'Ф',
+    s: 'і', S: 'І',
+    d: 'в', D: 'В',
+    l: 'д', L: 'Д',
+    ';': 'ж', ':': 'Ж',
+    "'": 'є', '"': 'Є',
+    z: 'я', Z: 'Я',
+    x: 'ч', X: 'Ч',
+    c: 'с', C: 'С',
+    b: 'и', B: 'И',
+    m: 'ь', M: 'Ь',
+    ',': 'б', '<': 'Б',
+    '.': 'ю', '>': 'Ю',
+    '/': '.', '?': ','
+};
+
+let string = 'ghbdsn';
+function replacer(string, keyboardMap) {
+    let newString = '';
+    for (const char of string) {
+        if (keyboardMap.hasOwnProperty(char)) {
+            newString += keyboardMap[char];
+        } else {
+            newString += char;
+        }
+    }
+    return newString;
+}
+// console.log(string);
+// console.log(replacer(string, keyboardMap));
+
+// let str = 'dsnfyyz';
+// console.log(str);
+// console.log(replacer(str, keyboardMap));
+
+
+let string2 = 'ghbdsn';
+console.log(string2);
+console.log(replacer(string2, keyboardMap));
+
+
+//gjzcytyyz
